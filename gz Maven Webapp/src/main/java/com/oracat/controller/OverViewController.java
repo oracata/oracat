0dcfaf667f5bfe1603e841dd42e1dfeffa86dceb
oracat <oracat@DESKTOP-9G9O7M8>
2019-08-02 15:40:14 +0800

    1111

--- gz Maven Webapp/src/main/java/com/oracat/controller/OverViewController.java
+++ gz Maven Webapp/src/main/java/com/oracat/controller/OverViewController.java
@@ -1,15 +1,23 @@
 package com.oracat.controller;
  
+import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
- 
-@Controller
-public class OverViewController {
- 
-    @RequestMapping("/overview")
-    public String overView(Model model) {         
-        model.addAttribute("greeting", "xxxxx");         
-        return"overview";         
-    } 
-}
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller  
+public class OverViewController {  
+
+    //@Resource  
+    //private UserService userService; 
+
+    @RequestMapping("/overview")    
+    public ModelAndView getIndex(){      
+        ModelAndView mav = new ModelAndView("overview"); 
+    //    User user = userService.selectUserById(1); 
+        mav.addObject("user", "dfsadfsdf"); 
+        return mav; 
+    }    
+}  
